1a5c44f65ff9d9eaaa1023913fca18b98b425254
kanthi Leela <137202626+KanthiLeela@users.noreply.github.com>
2024-07-02 13:55:28 +0530

    Add files via upload
    Tic-tac-toe is a fun game played by two players. Before starting the game, a 3x3 
    square grid is formed using two vertical and two horizontal lines. The player who succeeds in 
    placing three of their marks in a horizontal, vertical, or diagonal row is the winner. In this study, 
    we develop a computer program where a player can challenge artificial intelligent heuristic 
    approach (opponent) to win the game. We want to prove that artificial intelligent heuristic 
    approach enables in giving a great performance for development of tic-tac-toe game.

---
+++ TTT.js
@@ -0,0 +1,83 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg =document.querySelector("#msg");
+
+let turn0= true;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,8],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach ((box) => {
+    box.addEventListener("click", () => {
+       
+        if (turn0) {
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+const enableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = false;
+        box.innerText="";
+    }
+};
+const showWinner = (winner) => {
+    msg.innerText = `Congrats, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+const checkWinner = () =>
+    {
+    for (let pattern of winPatterns)
+    {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "")
+            {
+                if (pos1Val === pos2Val && pos2Val === pos3Val)
+                {
+                   
+                    showWinner(pos1Val);
+                }
+            }
+        
+        }
+    };
+
+    newGamebtn.addEventListener("click",resetGame);
+    resetBtn.addEventListener("click",resetGame);
+
+    
